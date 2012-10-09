fb9dde2bc085f9430a306eb9fd87963d0a113541
Frederica <frederica@meddik.com>
2012-10-09 17:57:18 -0400

    Initial Foundation commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoundationApp::Application.config.secret_token = 'aa0c199fe17d260bb2b3557ad93863f29ee2a70e3bf029cac329441bf31db5a2ca69f0d59272b38239a7cef84b579e715c81d3c309aa36cebd24073f80aedf9d'
